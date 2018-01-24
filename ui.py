d89bd05f5b6ea15062ad7814c9199708e76fe7df
Julie <jhchiu1@gmail.com>
2018-01-23 16:51:50 -0800

    Add files via upload

--- ui.py
+++ ui.py
@@ -1,31 +1,34 @@
-ui.py
-
 import db
 
+# Display menu options, validation added
 while True:
     choice = input("1. Add \n2. Remove \n3. View all \n4. Edit\n5. Quit\n")
 
     if choice != "1" and choice != "2" and choice != "3" and choice != "4" and choice != "5":
-        print("That wasn't a valid option, please try again.\n")
+        print("Please try again, enter a number between 1 and 5..\n")
 
+    # Ask user for juggler info to add
     elif choice == "1":
         name = input("Name?\n")
         country = input("Country?\n")
         catches = input("Catches?\n")
         db.add_juggler(name, country, catches)
 
+    # Ask user for juggler info to remove
     elif choice == "2":
         name = input("Name?\n")
         db.remove_juggler(name)
 
+    # Show all jugglers in db
     elif choice == "3":
-        db.view_all()
+        db.show_all()
 
-    # Assuming we are updating the highest number of catches
+    # Ask user to update high score for juggler named
     elif choice == "4":
         name = input("Name?\n")
         catches = input("New high score?\n")
         db.edit(name, catches)
 
+    # Exit
     elif choice == "5":
         break
